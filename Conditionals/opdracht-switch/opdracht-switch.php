31b06e3b3b840991d5a95fca409e1f9a57b6581f
YasminePlasmans <yasmineplasmans@gmail.com>
2014-11-19 11:22:46 +0100

    Opdracht switch

---
+++ Conditionals/opdracht-switch/opdracht-switch.php
@@ -0,0 +1,45 @@
+<?php
+
+	$getal = 5;
+	$dag = 'onbekende dag';
+
+	switch ($getal) {
+		case 1:
+			$dag = 'maandag';
+			break;
+		case 2:
+			$dag = 'dinsdag';
+			break;
+		case 3:
+			$dag = 'woensdag';
+			break;
+		case 4:
+			$dag = 'donderdag';
+			break;
+		case 5:
+			$dag = 'vrijdag';
+			break;
+		case 6:
+			$dag = 'zaterdag';
+			break;
+		case 7:
+			$dag = 'zondag';
+			break;
+		default:
+			$dag = 'onbekende dag';
+			break;
+	}
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Opdracht switch</title>
+</head>
+<body>
+	<h1>Opdracht Switch</h1>
+	<p>De dag die overeenkomt met het getal <?= $getal ?> is: <?= $dag ?></p>
+</body>
+</html>
